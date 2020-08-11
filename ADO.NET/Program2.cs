470b3f71bd8a6edbbc723f9a81a6d356ec91d73f
ricardocascan <ricardocascan@gmail.com>
2020-08-11 12:29:11 +0200

    asdf

--- ADO.NET/Program2.cs
+++ ADO.NET/Program2.cs
@@ -7,21 +7,17 @@ using System.Threading.Tasks;
 
 namespace ADO.NET
 {
-    class Program
+    class Program2
     {
         static void Main(string[] args)
         {
             SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\formacion\Documents\Facturacion.mdf;Integrated Security=True;Connect Timeout=30");
             connection.Open();
-            String sql = "select * from facturas";
+            String sql = "insert into facturas(numero, concepto) values(3, 'otra')";
             SqlCommand command = new SqlCommand(sql, connection);
-            SqlDataReader lector = command.ExecuteReader();
+            command.ExecuteNonQuery();
 
-            while (lector.Read())
-            {
-                Console.WriteLine(lector["numero"] + " " + lector["concepto"]);
-                
-            }
+            
 
             Console.ReadLine();
         }
